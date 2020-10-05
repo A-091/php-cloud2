4a44325ea579d0b6dff911bc13cd32d34ff9012f
A-091 <88.choice.r53@gmail.com>
2020-10-05 14:59:32 +0000

    profile2

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Profiles;
+use App\Profile;
 
 class ProfileController extends Controller
 {
